618ec1b4e970746da09d43884727d4e2d859f4a6
Yugandhar Dhore <143815412+Yug1020@users.noreply.github.com>
2026-04-20 23:41:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import simpy       
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Verve Tech Fest System", layout="wide")
 
